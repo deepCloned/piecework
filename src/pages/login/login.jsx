8a9df1b255da7e26fbc7b9be637a61f58a0a39cf
SummerTime <45481135+deepCloned@users.noreply.github.com>
2020-08-04 16:34:31 +0800

    首页+登录页静态页面

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,9 +1,134 @@
 import React, { Component } from "react";
 
+import "./login.scss";
+
 class Login extends Component {
-  state = {};
+  state = {
+    windowWidth: 0,
+    windowHeight: 0,
+    phone: "",
+    code: "",
+    timeLeft: 10,
+    lock: false, // 为 false 时禁止点击获取验证码
+    logoImg: 'https://piecework-server-product.oss-cn-hangzhou.aliyuncs.com/B7340AC613E9DB676863FA0AB485BBCA.png',
+    phoneImg: 'https://piecework-server-product.oss-cn-hangzhou.aliyuncs.com/C684FD5CE735A21914F70DD5EB4882A2.png',
+    codeImg: 'https://piecework-server-product.oss-cn-hangzhou.aliyuncs.com/81437C9D40C795F972DAAFC830D67E0D.png',
+  };
+
+  componentDidMount() {
+    // 获取屏幕高度和宽度
+    this.setWindowSize();
+  }
+
+  setWindowSize() {
+    const height = document.documentElement.clientHeight;
+    const width = document.documentElement.clientWidth;
+    this.setState({
+      windowHeight: height,
+      windowWidth: width,
+    });
+  }
+
+  handleInputPhone(e) {
+    e.persist();
+    this.setState({
+      phone: e.target.value,
+    });
+  }
+
+  handleInputCode(e) {
+    e.persist();
+    this.setState({
+      code: e.target.value,
+    });
+  }
+
+  /* 获取验证码 */
+  handleGetCode() {
+    const { lock } = this.state;
+    if (lock) {
+      console.log("请勿重复获取验证码");
+      return;
+    } else {
+      this.setState({
+        lock: true
+      })
+      console.log("获取验证码");
+      console.log('fetch is', fetch)
+    }
+  }
+
+  /* 提交登录 */
+  handleConfirmLogin() {
+    console.log("login");
+  }
+
   render() {
-    return <div className="login">login</div>;
+    const {
+      windowHeight,
+      windowWidth,
+      phone,
+      code,
+      timeLeft,
+      lock,
+      logoImg,
+      codeImg,
+      phoneImg,
+    } = this.state;
+
+    return (
+      <div
+        className="login"
+        style={{
+          width: windowWidth,
+          height: windowHeight,
+        }}
+      >
+        <div className="logo">
+          <img className="logo-image" src={logoImg} alt="logo"></img>
+          <span className="app-name">管单单</span>
+        </div>
+        <form className="login-area">
+          <div className="input-common phone">
+            <img src={phoneImg} alt="phone"></img>
+            <input
+              type="text"
+              value={phone}
+              placeholder="请输入手机号"
+              onChange={this.handleInputPhone.bind(this)}
+            ></input>
+          </div>
+          <div className="input-common code">
+            <img src={codeImg} alt="code"></img>
+            <input
+              type="text"
+              value={code}
+              placeholder="请输入验证码"
+              onChange={(e) => {
+                this.handleInputCode(e);
+              }}
+            ></input>
+            <span
+              className={`get-code ${lock ? 'is-lock' : ''}`}
+              onClick={() => {
+                this.handleGetCode();
+              }}
+            >
+              {lock ? timeLeft + "秒后重新发送" : "获取验证码"}
+            </span>
+          </div>
+          <input
+            type="button"
+            value="登录"
+            className="confirm"
+            onClick={() => {
+              this.handleConfirmLogin();
+            }}
+          ></input>
+        </form>
+        <div className="slogan">-- 让管理更高效 --</div>
+      </div>
+    );
   }
 }
 
--- src/pages/welcome/welcome.jsx
+++ src/pages/welcome/welcome.jsx
@@ -1,9 +1,51 @@
 import React, { Component } from "react";
 
+import "./welcome.scss";
+
 class Welcome extends Component {
-  state = {};
+  state = {
+    windowWidth: 0,
+    windowHeight: 0,
+  };
+
+  componentDidMount() {
+    // 获取屏幕高度和宽度
+    this.setWindowSize();
+  }
+
+  setWindowSize() {
+    const height = document.documentElement.clientHeight;
+    const width = document.documentElement.clientWidth;
+    this.setState({
+      windowHeight: height,
+      windowWidth: width,
+    });
+  }
+
+  /* 跳转页面 */
+  handleGoLogin(e) {
+    this.props.history.push(`/login`);
+  }
+
   render() {
-    return <div className="welcome">welcome</div>;
+    return (
+      <div
+        className="welcome"
+        style={{
+          width: this.state.windowWidth,
+          height: this.state.windowHeight,
+        }}
+      >
+        <input
+          className="login-tip"
+          type="button"
+          value="快捷登录"
+          onClick={() => {
+            this.handleGoLogin();
+          }}
+        />
+      </div>
+    );
   }
 }
 
